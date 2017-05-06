b097285220016c8484d1381043551e7a1e032fa3
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-07 01:11:36 +0300

    Add private properties
    *Add name and id properties in the Stylist.java file

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,3 +1,12 @@
 import java.util.List;
 import java.util.ArrayList;
 import org.sql2o.*;
+
+public class Stylist {
+  private String name;
+  private int id;
+
+  public Category(String name) {
+      this.name = name;
+  }
+}
