3822e1689754352d1f043fb2fb62ce433265b0e2
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-07 02:16:19 +0300

    Add void method save() that saves added stylists names

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -55,4 +55,14 @@ public class Stylist {
         }
 }
 
+
+public void save() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "INSERT INTO categories(name) VALUES (:name)";
+        this.id = (int) con.createQuery(sql, true)
+          .addParameter("name", this.name)
+          .executeUpdate()
+          .getKey();
+        }
+      }
 }
