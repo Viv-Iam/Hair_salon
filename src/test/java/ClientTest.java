8e3edc58daf7c1281cf15ec0c687e23b91c30542
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-06 23:58:48 +0300

    Add method setUp to run before each test

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -3,6 +3,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
 
-public class TaskTest {
-
+public class ClientTest {
+  @Before
+      public void setUp() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "postgres", "postgres");
+      }
 }
