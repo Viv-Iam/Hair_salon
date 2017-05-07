aa7c068fd71af628157af32e608d52cc9696cc26
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-07 11:58:50 +0300

    Add get method in App.java that enables adding of new stylists

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,6 +27,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/stylists/new", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("template", "templates/stylist-form.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+
     post("/stylists", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
   String name = request.queryParams("name");
