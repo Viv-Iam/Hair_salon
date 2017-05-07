3371be120e18b9cf539d5a5f6ca5802267f8e55d
Viv-Iam <vivian.opondoh@gmail.com>
2017-05-07 04:59:00 +0300

    Add variable
    *Add layout variable thats equal to "templates/layout.vtl"

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    ProcessBuilder process = new ProcessBuilder();
+         Integer port;
+         if (process.environment().get("PORT") != null) {
+             port = Integer.parseInt(process.environment().get("PORT"));
+         } else {
+             port = 4567;
+         }
+
+        setPort(port);
+
+        staticFileLocation("/public");
+            String layout = "templates/layout.vtl";
